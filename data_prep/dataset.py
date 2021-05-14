71d8247f7f988362bc0bcf67d8eba40753c14e2f
MatPrst <parisot.mathias.31@gmail.com>
2021-05-14 14:52:14 +0200

    Add imdb graph

--- data_prep/dataset.py
+++ data_prep/dataset.py
@@ -310,3 +310,89 @@ class HuggingFaceDatasetText(TextDataset):
     def __len__(self):
         return len(self.labels)
 
+
+from datasets import load_dataset
+from nltk.tokenize.regexp import WordPunctTokenizer
+
+
+class HuggingFaceGraph(GraphDataset):
+    """
+    Parent class HuggingFace graph datasets. To use this class, 
+    the underlying HuggingFace dataset must have a text and a label attribute.
+    """
+    def __init__(self, device, val_size=0.1, n_train_docs=None, dataset_name="ag_news"):
+        """
+        Creates the train, test, and validation splits.
+        Args:
+            device (Device): Device to use to store the dataset.
+            val_size (float, optional): Proportion of training documents to include in the validation set.
+            n_train_docs (int, optional): Number of documents to use from the training set. If None, include all.
+        Returns:
+            train_split (Dataset): Training split.
+            test_split (Dataset): Test split.
+            val_split (Dataset): Validation split.
+        """
+        super(HuggingFaceGraph, self).__init__(device, n_train_docs)
+
+        docs, labels, classes = self.prepare(val_size, n_train_docs, dataset_name)
+
+        train_labels, test_labels, val_labels = labels
+        self.all_labels = train_labels + test_labels + val_labels
+
+        self.tokenizer = WordPunctTokenizer()  # TODO: look into a better one?
+
+        super(HuggingFaceGraph, self).initialize_data(docs, classes)
+
+    @staticmethod
+    def prepare(val_size=0.1, n_train_docs=None, dataset_name="ag_news"):
+        """
+        Return the training, validation, and tests splits along with the classes of the dataset.
+        Args:
+            val_size (float, optional): Proportion of training documents to include in the validation set.
+            n_train_docs (int, optional): Number of documents to use from the training set. If None, include all.
+        Returns:
+            splits (tuple): Tuple containing 3 list of strings for training, test, and validation datasets.
+            labels (tuple): Tuple containing 3 list of labels for training, test, and validation datasets.
+            unique_classes (List): List of Strings containing the class names.
+        """
+        # download the dataset
+        dataset = load_dataset(dataset_name)
+
+        # create the training and validation splits
+        train_val_splits = dataset["train"].train_test_split(test_size=val_size)
+
+        # extract the text for each news article        
+        train_texts, train_labels = zip(*[(data["text"], data["label"]) for data in train_val_splits["train"]])
+        val_texts, val_labels = zip(*[(data["text"], data["label"]) for data in train_val_splits["test"]])
+        test_texts, test_labels = zip(*[(data["text"], data["label"]) for data in dataset["test"]])
+
+        unique_classes = train_val_splits["train"].features["label"].names
+
+        if n_train_docs is not None:
+            # For testing with only a few docs:
+            n_test_val_docs = int(val_size * n_train_docs)
+            docs = (list(train_texts[:n_train_docs]), list(test_texts[:n_test_val_docs]),
+                    list(val_texts[:n_test_val_docs]))
+            labels = (list(train_labels[:n_train_docs]), list(test_labels[:n_test_val_docs]),
+                      list(val_labels[:n_test_val_docs]))
+        else:
+            docs = (list(train_texts), list(test_texts), list(val_texts))
+            labels = (list(train_labels), list(test_labels), list(val_labels))
+
+        return docs, labels, unique_classes
+
+    def get_label_node_mapping(self):
+        return self.all_labels + [-1] * self.n_words
+
+    def pre_process_words(self, all_docs):
+        return [self.tokenizer.tokenize(sentence) for sentence in all_docs]
+
+    @property
+    def num_classes(self):
+        return len(self.itol)
+
+    def __getitem__(self, idx):
+        return self.data
+
+    def __len__(self):
+        return 1
--- data_prep/imdb_graph.py
+++ data_prep/imdb_graph.py
@@ -1,9 +1,9 @@
 from data_prep.dataset import HuggingFaceGraph
 
-class AGNewsGraph(HuggingFaceGraph):
+class IMDbGraph(HuggingFaceGraph):
     def __init__(self, device, val_size=0.1, n_train_docs=None):
         """
-        Creates the train, test, and validation splits for AGNews.
+        Creates the train, test, and validation splits for IMDb.
         Args:
             device (Device): Device to use to store the dataset.
             val_size (float, optional): Proportion of training documents to include in the validation set.
@@ -13,4 +13,4 @@ class AGNewsGraph(HuggingFaceGraph):
             test_split (Dataset): Test split.
             val_split (Dataset): Validation split.
         """
-        super(AGNewsGraph, self).__init__(device, val_size=val_size, n_train_docs=n_train_docs, dataset_name="ag_news")
+        super(IMDbGraph, self).__init__(device, val_size=val_size, n_train_docs=n_train_docs, dataset_name="imdb")
