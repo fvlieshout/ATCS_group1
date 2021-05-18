d47484534cdd30d3b7defdb27b2b7ab966f094fb
fvlieshout <floorvanlieshout@hotmail.com>
2021-05-18 16:06:15 +0200

    generate doc_features in batches iso one by one

--- data_prep/roberta_graph_dataset.py
+++ data_prep/roberta_graph_dataset.py
@@ -54,17 +54,23 @@ class RobertaGraphDataset(GraphDataset):
 
         with torch.no_grad():
             print('Generating document node features')
-            for doc in self._raw_texts:
-                encoding = self._tokenizer(doc, truncation=True, padding=True)['input_ids']
+            batch_size = 64
+            num_batches = int(len(self._raw_texts)/batch_size)+1
+            for i in range(num_batches):
+                if i == num_batches-1:
+                    docs = self._raw_texts[i*batch_size:]
+                else:
+                    docs = self._raw_texts[i*batch_size:(i+1)*batch_size]
+                encoding = self._tokenizer(docs, truncation=True, padding=True)['input_ids']
                 encoding = torch.tensor(encoding, dtype=torch.long, device=self._device)
                 encoding = doc_embedder(encoding)[1]
                 features_docs.append(encoding)
-            features_docs = torch.stack(features_docs).to(self._device)
+            features_docs = torch.cat(features_docs, dim=0).to(self._device)
 
             print('Generating word node features')
             for token in self._tokens:
                 embed_token = token_embedder[token]
                 features_words.append(embed_token)
-        features_words = torch.stack(features_words).to(self._device)
+            features_words = torch.stack(features_words).to(self._device)
 
         return features_docs, features_words
