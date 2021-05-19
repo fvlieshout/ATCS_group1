2a953cf0e009501f65087ae765d1dd325d01d585
Rahel Habacker <rahel@habacker.de>
2021-05-19 12:08:07 +0200

    Renaming epoch in cosine scheduler to current step, because thiis is what it really is.

--- models/model.py
+++ models/model.py
@@ -131,13 +131,13 @@ class CosineWarmupScheduler(optim.lr_scheduler._LRScheduler):
         super().__init__(optimizer)
 
     def get_lr(self):
-        lr_factor = self.get_lr_factor(epoch=self.last_epoch)
+        lr_factor = self.get_lr_factor(current_step=self.last_epoch)
         return [base_lr * lr_factor for base_lr in self.base_lrs]
 
-    def get_lr_factor(self, epoch):
-        lr_factor = 0.5 * (1 + np.cos(np.pi * epoch / self.max_num_iters))
-        if epoch <= self.warmup:
-            lr_factor *= epoch * 1.0 / self.warmup
+    def get_lr_factor(self, current_step):
+        lr_factor = 0.5 * (1 + np.cos(np.pi * current_step / self.max_num_iters))
+        if current_step <= self.warmup:
+            lr_factor *= current_step * 1.0 / self.warmup
         return lr_factor
 
 
