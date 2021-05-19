7fbbc9ba2f9d5f113b8bf08d4c16dff128b6110e
Rahel Habacker <rahel@habacker.de>
2021-05-19 23:12:15 +0200

    Using self.last_epoch directly in get_lr_factor.

--- models/model.py
+++ models/model.py
@@ -145,10 +145,11 @@ class CosineWarmupScheduler(optim.lr_scheduler._LRScheduler):
         super().__init__(optimizer)
 
     def get_lr(self):
-        lr_factor = self.get_lr_factor(current_step=self.last_epoch)
+        lr_factor = self.get_lr_factor()
         return [base_lr * lr_factor for base_lr in self.base_lrs]
 
-    def get_lr_factor(self, current_step):
+    def get_lr_factor(self):
+        current_step = self.last_epoch
         lr_factor = 0.5 * (1 + np.cos(np.pi * current_step / self.max_num_iters))
         if current_step <= self.warmup:
             lr_factor *= current_step * 1.0 / self.warmup
