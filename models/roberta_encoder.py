80440a9527185e2f8ae9a0934d71bd195be0c585
Matyasch <schubertmatyas@gmail.com>
2021-05-19 02:16:27 +0200

    Fixing number of frozen layers

--- models/roberta_encoder.py
+++ models/roberta_encoder.py
@@ -14,7 +14,7 @@ class RobertaEncoder(nn.Module):
 
         # freezing the encoder parameters from half of the layers
         encoder_layers = self.model.encoder.layer
-        for layer in encoder_layers[:len(encoder_layers) / 2]:
+        for layer in encoder_layers[:int(len(encoder_layers) / 2)]:
             for param in layer.parameters():
                 param.requires_grad = False
 
