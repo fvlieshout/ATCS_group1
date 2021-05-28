dda2d9eae820f2f9725027c9b469ee34c6db98d8
Matyasch <schubertmatyas@gmail.com>
2021-05-28 13:10:22 +0200

    Remove print

--- data_prep/graph_dataset.py
+++ data_prep/graph_dataset.py
@@ -68,8 +68,6 @@ class GraphDataset(Dataset, GeometricDataset):
         self._data.val_mask = val_mask
         self._data.test_mask = test_mask
 
-        print(data)
-
     def labels(self):
         """
         Return the labels of data points.
